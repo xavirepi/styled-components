e3f059d548aaff3013f2ae748810c9fa5b5e5042
Javier Repilado <javier.repilado@Javiers-MacBook-Pro.local>
2021-07-03 17:15:59 +0200

    add Heading and Button Styled Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-size: 2rem;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  border-radius: 4px;
+  border: none;
+  color: $white; 
+  font-size: 2rem;
+  background: ${({ type }) => type === 'cancel' ? 'tomato' : 'indigo'}
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Heading>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Heading>
+        <Button type="cancel">Cancel</Button>
+        <Button type="save">Save</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
