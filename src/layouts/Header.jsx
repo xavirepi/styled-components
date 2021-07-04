0d7cfc9ae4d135e206e15ca6cbaf849eae9455ff
Javier Repilado <javier.repilado@Javiers-MacBook-Pro.local>
2021-07-04 10:19:45 +0200

    scope to Header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import logo from '../logo.svg';
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../logo.svg'
+
+const Header = ({ className }) => {
+  return (
+    <header className={className}>
+      <img src={logo} alt='logo' className='logo' />
+    </header>
+  )
+};
 
 // It could be modularized into an external directory but it'd only make sense if this component would be used somewhere else.
 // As it's not the case it is styled here in the same file.
-const AppHeader = styled.header`
+export default styled(Header)`
   background: #282c34;
   padding: 10px 5%;
   .logo {
     width: 60px;
   }
-`
-
-const Header = () => {
-  return (
-    <AppHeader>
-      <img src={logo} alt='logo' className="logo"/>
-    </AppHeader>
-  )
-}
-
-export default Header;
+`;
